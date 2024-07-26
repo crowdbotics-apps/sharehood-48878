44738cf9be672dd7aac16df106989879e76915bc
Crowdbotics <team@crowdbotics.com>
2024-07-26 16:57:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sharehood-48878",
+  url: "https://sharehood-48878.botics.co",
+  api: "https://sharehood-48878.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
